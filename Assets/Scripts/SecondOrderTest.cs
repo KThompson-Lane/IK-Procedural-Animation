abeec533734237f8548d15f923154f491428faa6
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-04-19 18:54:16 +0100

    add example SecondOrder motion

--- Assets/Scripts/SecondOrderTest.cs
+++ Assets/Scripts/SecondOrderTest.cs
@@ -2,48 +2,38 @@
 
 namespace DefaultNamespace
 {
-    public class SecondOrderTest : MonoBehaviour
+    public class SecondOrderMotion
     {
         private Vector3 previousInput;
-        private Vector3 yd;
+        private Vector3 output, previousOutput;
         private float k1, k2, k3;
-
-        //  F corresponds to the frequency of the motion system
-        //  Z is the damping coefficient
-        //  R initial response of the system.
-        public float f, z, r;
-        public Transform target;
-        private void Awake()
+        
+        public SecondOrderMotion(float f, float z, float r, Vector3 initialInput)
         {
-            CalculateKValues();
+            previousInput = initialInput;
+            output = initialInput;
+            previousOutput = initialInput;
+            //  F corresponds to the frequency of the motion system
+            //  Z is the damping coefficient
+            //  R initial response of the system.
+            CalculateKValues(f,z,r);
         }
 
-        private void OnValidate()
-        {
-            CalculateKValues();
-        }
-
-        private void CalculateKValues()
+        public void CalculateKValues(float f, float z, float r)
         {
             k1 = z / (Mathf.PI * f);
             k2 = 1 / ((2 * Mathf.PI * f) * (2 * Mathf.PI * f));
             k3 = r * z / (2 * Mathf.PI * f);
-
-            previousInput = target.position;
-            yd = Vector3.zero;
         }
-        
-        private void FixedUpdate()
+
+        public Vector3 Update(float T, Vector3 x)
         {
-            Vector3 x = target.position;
-            float T = Time.deltaTime;
             Vector3 xd = (x - previousInput) / T;
             previousInput = x;
 
-            Vector3 y = transform.position;
-            y = y + T * yd;
-            yd = yd + T * (x + k3 * xd - y - k1 * yd) / k2;
-            transform.position = y;
+            output = output + T * previousOutput;
+            previousOutput = previousOutput + T * (x + k3 * xd - output - k1 * previousOutput) / k2;
+            return output;
         }
     }
 }
